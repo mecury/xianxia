fbbaad9526d36efc88feebff3e52a1d0e2c4f2dc
mecury <183941826@qq.com>
2016-05-19 21:36:03 +0800

    add HttpUtil

---
+++ app/src/main/java/com/mecuryli/xianxia/support/HttpUtil.java
@@ -0,0 +1,45 @@
+package com.mecuryli.xianxia.support;
+
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * Created by 海飞 on 2016/5/19.
+ */
+public class HttpUtil {
+    private static final OkHttpClient mOkHttpClient = new OkHttpClient();
+    static {
+        mOkHttpClient.setConnectTimeout(30, TimeUnit.SECONDS);
+        mOkHttpClient.setReadTimeout(30, TimeUnit.SECONDS);
+        mOkHttpClient.setWriteTimeout(30,TimeUnit.SECONDS);
+    }
+
+    //不开启异步线程访问网络
+    public static Response execute(Request request) throws IOException {
+        return mOkHttpClient.newCall(request).execute();
+    }
+    //开启异步线程访问网络
+    public static void enqueue(Request request, Callback responseCallback){
+        mOkHttpClient.newCall(request).enqueue(responseCallback);
+    }
+    //开启异步线程访问网络，且不在意返回结果（实现空callback）
+    public static void enqueue(Request request){
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+
+            }
+        });
+    }
+}
